f704fcb58020237d786e0d192ef96689fb64a569
matteo <matteo@dell.homenet.telecomitalia.it>
2016-05-04 22:50:00 +0200

    verbo avere

---
+++ 020_verbi_avere.pl
@@ -0,0 +1,21 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+coniugazione_verbo('indicativo', 'presente', 'avere', ['ho', 'hai', 'ha', 'abbiamo', 'avete', 'anno']).
+coniugazione_verbo('indicativo', 'passato remoto', 'avere', ['ebbi', 'avesti', 'ebbe', 'avemmo', 'aveste', 'ebbero']).
+coniugazione_verbo('indicativo', 'futuro semplice', 'avere', ['avrò', 'avrai', 'avrà', 'avremo', 'avrete', 'avranno']).
+coniugazione_verbo('congiuntivo', 'presente', 'avere', ['abbia', 'abbia', 'abbia', 'abbiamo', 'abbiate', 'abbiamo']).
+coniugazione_verbo('imperativo', 'presente', 'avere', ['--', 'abbi', 'abbia', 'abbiamo', 'abbiate', 'abbiamo']).
+
